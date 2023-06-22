f44a5ee291cb9e47749902bf2b377774c97418fd
grayolaRSA <gtndanga@gmail.com>
2023-06-22 20:36:24 +0200

    update

--- models/city.py
+++ models/city.py
@@ -3,10 +3,16 @@
 from models.base_model import BaseModel
 from models.base_model import Base
 from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 
 class City(BaseModel, Base):
     """ The city class, contains state ID and name """
     __tablename__ = 'cities'
-    name = Column(String(128), nullable=False)
-    state_id = Column(String(60), nullable=False, ForeignKey('states.id'))
+    if models.storage_t = "db":
+        name = Column(String(128), nullable=False)
+        state_id = Column(String(60), nullable=False, ForeignKey('states.id'))
+        places = relationship("Place", cascade="all, delete", backref="cities")
+    else:
+        name = ''
+        state_id = ''
--- models/place.py
+++ models/place.py
@@ -1,18 +1,43 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, Integer, String, ForeignKey
 
 
-class Place(BaseModel):
+class Place(BaseModel, Base):
     """ A place to stay """
-    city_id = ""
-    user_id = ""
-    name = ""
-    description = ""
-    number_rooms = 0
-    number_bathrooms = 0
-    max_guest = 0
-    price_by_night = 0
-    latitude = 0.0
-    longitude = 0.0
-    amenity_ids = []
+    __tablename__ = 'places'
+    if models.storage_t = "db":
+        city_id = Column(String(60), nullable=False, ForeignKey('cities.id'))
+        user_id = Column(String(60), nullable=False. ForeignKey('users.id'))
+        name = Column(String(128), nullable=False)
+        description = Column(String(1024), nullable=False)
+        number_rooms = Column(int, nullable=False, default=0)
+        number_bathrooms = Column(int, nullable=False, default=0)
+        max_guest = Column(int, nullable=False, default=0)
+        price_by_night = Column(int, nullable=False, default=0)
+        latitude = Column(float, nullable=False)
+        longitude = Column(float, nullable=False)
+        reviews = relationship("Review", cascade="all, delete",
+                               backref="place")
+    else:
+        city_id = ""
+        user_id = ""
+        name = ""
+        description = ""
+        number_rooms = 0
+        number_bathrooms = 0
+        max_guest = 0
+        price_by_night = 0
+        latitude = 0.0
+        longitude = 0.0
+        amenity_ids = []
+        @setter
+        def reviews(self)
+        "getter for list of instances related to the place"
+        places_list = []
+        all_places = models.storage.all(Place)
+        for places in all_places.values():
+            if places.place_id = self.id:
+                places_list.append(places)
+                return places_list
