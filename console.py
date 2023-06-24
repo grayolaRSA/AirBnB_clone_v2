604b79d3a7ef23cb2998dd3884c9ae90e6c7b185
grayolaRSA <gtndanga@gmail.com>
2023-06-25 01:08:06 +0200

    update

--- console.py
+++ console.py
@@ -2,6 +2,7 @@
 """Console Module"""
 import cmd
 import sys
+import shlex
 import models
 from models import storage
 from models.base_model import BaseModel
@@ -12,6 +13,7 @@ from models.place import Place
 from models.review import Review
 from models.state import State
 from models.user import User
+from models.engine.file_storage import FileStorage
 
 classes = {"Amenity": Amenity, "BaseModel": BaseModel, "City": City,
            "Place": Place, "Review": Review, "State": State, "User": User}
@@ -20,26 +22,54 @@ classes = {"Amenity": Amenity, "BaseModel": BaseModel, "City": City,
 class HBNBCommand(cmd.Cmd):
     prompt = "(hbnb) "
 
+    classes = {"Amenity": Amenity, "BaseModel": BaseModel, "City": City,
+               "Place": Place, "Review": Review, "State": State, "User": User}
+
     def emptyline(self):
         """Handles empty line"""
         pass
 
-    def do_quit(self, line):
+    def do_quit(self, arg):
         """Quit command to exit the program"""
         return True
 
-    def do_create(self, line):
-        """Create a new instance of a class"""
-        if not line:
-            print("** class name missing **")
-        elif line not in self.classes:
-            print("** class doesn't exist **")
-        else:
-            new_instance = self.classes[line]()
-            new_instance.save()
-            print(new_instance.id)
+    def _key_value_parser(self, args):
+        """creates a dictionary from a list of strings"""
+        new_dict = {}
+        for arg in args:
+            if "=" in arg:
+                kvp = arg.split('=', 1)
+                key = kvp[0]
+                value = kvp[1]
+                if value[0] == value[-1] == '"':
+                    value = shlex.split(value)[0].replace('_', ' ')
+                else:
+                    try:
+                        value = int(value)
+                    except ValueError:
+                        try:
+                            value = float(value)
+                        except ValueError:
+                            continue
+                new_dict[key] = value
+        return new_dict
 
-    def do_show(self, line):
+    def do_create(self, arg):
+        """Create a new instance of a class"""
+        args = arg.split()
+        if len(args) == 0:
+            print("** class name missing **")
+            return False
+        if args[0] in classes:
+            new_dict = self._key_value_parser(args[1:])
+            instance = classes[args[0]](**new_dict)
+        else:
+            print("** class doesn't exist **")
+            return False
+        print(instance.id)
+        instance.save()
+
+    def do_show(self, arg):
         """Prints the string representation of an instance"""
         args = line.split()
         if not line:
@@ -56,7 +86,7 @@ class HBNBCommand(cmd.Cmd):
             else:
                 print("** no instance found **")
 
-    def do_destroy(self, line):
+    def do_destroy(self, arg):
         """Deletes an instance based on the class name and id"""
         args = line.split()
         if not line:
@@ -74,26 +104,26 @@ class HBNBCommand(cmd.Cmd):
             else:
                 print("** no instance found **")
 
-    def do_all(self, line):
+    def do_all(self, arg):
         """Prints all string representations of instances"""
         objects = FileStorage().all()
         objects_list = []
-        if not line:
+        if not arg:
             for key in objects:
                 objects_list.append(str(objects[key]))
             print(objects_list)
-        elif line in self.classes:
+        elif arg in self.classes:
             for key in objects:
-                if key.split(".")[0] == line:
+                if key.split(".")[0] == arg:
                     objects_list.append(str(objects[key]))
             print(objects_list)
         else:
             print("** class doesn't exist **")
 
-    def do_update(self, line):
+    def do_update(self, arg):
         """Updates an instance based on the class name and id"""
-        args = line.split()
-        if not line:
+        args = arg.split()
+        if not arg:
             print("** class name missing **")
         elif args[0] not in self.classes:
             print("** class doesn't exist **")
@@ -119,7 +149,7 @@ class HBNBCommand(cmd.Cmd):
             else:
                 print("** no instance found **")
 
-    def do_EOF(self, line):
+    def do_EOF(self, arg):
         """Handles end-of-file (EOF) signal"""
         return True
 
--- models/base_model.py
+++ models/base_model.py
@@ -3,13 +3,13 @@ from datetime import datetime
 from sqlalchemy import Column, Integer, String, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 
-if models.storage_t == "db":
-    Base = declarative_base()
-else:
-    Base = object
+if __name__ == '__main__':
+    from models import storage
+
+Base = declarative_base()
 
 
-class BaseModel(Base):
+class BaseModel:
     """A base class for all hbnb models"""
     id = Column(String(60), nullable=False, primary_key=True)
     created_at = Column(DateTime, nullable=False, default=datetime.utcnow)
@@ -23,10 +23,16 @@ class BaseModel(Base):
             self.updated_at = datetime.now()
         else:
             self.id = str(uuid.uuid4())
-            self.created_at = datetime.strptime(kwargs['created_at'],
-                                                '%Y-%m-%dT%H:%M:%S.%f')
-            self.updated_at = datetime.strptime(kwargs['updated_at'],
-                                                '%Y-%m-%dT%H:%M:%S.%f')
+            if 'created_at' in kwargs:
+                self.created_at = datetime.strptime(kwargs['created_at'],
+                                                    '%Y-%m-%dT%H:%M:%S.%f')
+            else:
+                self.created_at = datetime.now()
+                if 'updated_at' in kwargs:
+                    self.updated_at = datetime.strptime(kwargs['updated_at'],
+                                                        '%Y-%m-%dT%H:%M:%S.%f')
+                else:
+                    self.updated_at = datetime.now()
 
     def __str__(self):
         """Returns a string representation of the instance"""
@@ -35,16 +41,21 @@ class BaseModel(Base):
 
     def save(self):
         """Updates updated_at with current time when instance is changed"""
-        from models import storage
-        self.updated_at = datetime.now()
-        storage.new(self)
-        storage.save()
+        if __name__ == '__main__':
+            storage.new(self)
+            storage.save()
+        else:
+            from models import storage
+            storage.new(self)
+            storage.save()
 
     def to_dict(self):
         """Converts instance into dict format"""
         dictionary = self.__dict__.copy()
         dictionary['__class__'] = type(self).__name__
-        dictionary['created_at'] = self.created_at.isoformat()
-        dictionary['updated_at'] = self.updated_at.isoformat()
+        if self.created_at:
+            dictionary['created_at'] = self.created_at.isoformat()
+        if self.updated_at:
+            dictionary['updated_at'] = self.updated_at.isoformat()
         dictionary.pop('_sa_instance_state', None)
         return dictionary
