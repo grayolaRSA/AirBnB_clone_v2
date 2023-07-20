a26cd140d5b565cf5db19d913de28a79093d9bfc
grayolaRSA <gtndanga@gmail.com>
2023-07-20 22:55:37 +0200

    update

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,36 +1,33 @@
 #!/usr/bin/python3
-# module for fabric script that creates tar archive
+# Fabric script to create tar archive of web static folder
 
-from fabric.api import run
+from fabric.api import local
 from datetime import datetime
-import tarfile
 import os
-import glob
-
-# get current date
-current_date = datetime.now().strftime("%Y%m%d%H%M%S")
-
-# set filenames
-dump_file = "/web_static/*"
-archive_file = f"versions/web_static_{current_date}.tgz"
 
 
 def do_pack():
-    # Create new directory if not present
+    # Create "versions" directory if not present
     new_directory = "versions"
     if not os.path.exists(new_directory):
         os.mkdir(new_directory)
 
-    # Get files list using glob
-    files = glob.glob(dump_file)
+    # Get the current date and time in the desired format
+    current_date = datetime.now().strftime("%Y%m%d%H%M%S")
 
-    # Create compressed archive
-    with tarfile.open(archive_file, "w:gz") as tar:
-        for file in files:
-            print(f"Packing {file}")
-            tar.add(file, arcname=os.path.basename(file))
+    # Generate the archive filename
+    archive_file = f"{new_directory}/web_static_{current_date}.tgz"
 
-    # Get the size of the archive file
-    archive_size = os.path.getsize(archive_file)
+    # Create the compressed archive using the "tar" command
+    command = f"tar -czvf {archive_file} web_static"
+    result = local(command)
 
-    print(f"web_static packed: {archive_file} -> {archive_size} bytes")
+    # Check if the archive has been correctly generated
+    if result.succeeded:
+        # Get the size of the archive file
+        archive_size = os.path.getsize(archive_file)
+
+        print(f"web_static packed: {archive_file} -> {archive_size} bytes")
+        return archive_file
+    else:
+        return None
