db72e0a3b191bd5ed846a363e1b85c27706739ac
grayolaRSA <gtndanga@gmail.com>
2023-07-24 00:00:31 +0200

    update

--- models/place.py
+++ models/place.py
@@ -7,6 +7,15 @@ import models
 from os import getenv
 from sqlalchemy.orm import relationship
 
+if models.storage_t = 'db':
+    place_amenity = Table('place_amenity', Base.metadata,
+                          Column('place_id', String(60),
+                                 ForeignKey('places.id'), nullable=False,
+                                 primary_key=True),
+                          Column('amenity_id', String(60),
+                                 ForeignKey('amenities.id'),
+                                 nullable=False, primary_key=True))
+
 
 class Place(BaseModel, Base):
     """ A place to stay """
@@ -26,13 +35,6 @@ class Place(BaseModel, Base):
         longitude = Column(Float, nullable=True)
         reviews = relationship("Review", cascade="all, delete",
                                backref="place")
-        place_amenity = Table('place_amenity', Base.metadata,
-                              Column('place_id', String(60),
-                                     ForeignKey('places.id'), nullable=False,
-                                     primary_key=True),
-                              Column('amenity_id', String(60),
-                                     ForeignKey('amenities.id'),
-                                     nullable=False, primary_key=True))
         amenities = relationship("Amenity", secondary="place_amenity",
                                  viewonly=False,
                                  back_populates='place_amenities')
