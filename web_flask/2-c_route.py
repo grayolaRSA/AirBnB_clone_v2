3e3294f90e56dd6988155f22a0e5584ec5087a22
grayolaRSA <gtndanga@gmail.com>
2023-07-23 11:36:07 +0200

    update

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""minimal flask web application"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/", strict_slashes=False)
+def hello_world():
+    """ returns welcome msg"""
+    return 'Hello HBNB!'
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """returns new msg"""
+    return 'HBNB'
+
+@app.route("/c/<text>", strict_slashes=False)
+def cisfun(text):
+    """display “C ” followed by text input"""
+    return 'C ' + text.replace('_', ' ')
+
+if __name__ == '__main__':
+    # Change the host parameter from '127.0.0.1' to '0.0.0.0'
+    app.run(host='0.0.0.0', port=5000)
