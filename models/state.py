ac02755364078494801d49970cab928801370191
grayolaRSA <gtndanga@gmail.com>
2023-07-23 15:05:51 +0200

    update

--- models/state.py
+++ models/state.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
-from models.base_model import Base
+from models.base_model import BaseModel, Base
+from models.city import City
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import relationship
 import models
