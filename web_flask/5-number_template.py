9b45664d60fed5cece6c8a428bc72d59ef59cccd
grayolaRSA <gtndanga@gmail.com>
2023-07-24 23:17:06 +0200

    update

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -5,20 +5,24 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route("/", strict_slashes=False)
 def hello_world():
     """ returns welcome msg"""
     return 'Hello HBNB!'
+
 
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
     """returns new msg"""
     return 'HBNB'
 
+
 @app.route("/c/<text>", strict_slashes=False)
 def cisfun(text):
     """display “C ” followed by text input"""
     return 'C ' + text.replace('_', ' ')
+
 
 @app.route('/python', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
@@ -26,15 +30,18 @@ def pythoniscool(text='is cool'):
     """displays word Python followed by text input"""
     return 'Python ' + text.replace('_', ' ')
 
+
 @app.route('/number/<int:n>', strict_slashes=False)
 def isanumber(n):
     """displays statement only if integer input received"""
     return f'{n} is a number'
 
+
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def numbersandtemplates(n):
-    """display a HTML page only if n is an integer"""
+    """display a HTML page only if integer input received"""
     return render_template('5-number.html', n=n)
+
 
 if __name__ == '__main__':
     # Change the host parameter from '127.0.0.1' to '0.0.0.0'
