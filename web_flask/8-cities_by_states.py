97526db6835bb151e48558b01691811ce68ab76b
grayolaRSA <gtndanga@gmail.com>
2023-07-29 01:19:56 +0200

    update

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -7,60 +7,6 @@ from models import storage
 app = Flask(__name__)
 
 
-@app.route("/", strict_slashes=False)
-def hello_world():
-    """ returns welcome msg"""
-    return 'Hello HBNB!'
-
-
-@app.route("/hbnb", strict_slashes=False)
-def hbnb():
-    """returns new msg"""
-    return 'HBNB'
-
-
-@app.route("/c/<text>", strict_slashes=False)
-def cisfun(text):
-    """display “C ” followed by text input"""
-    return 'C ' + text.replace('_', ' ')
-
-
-@app.route('/python', strict_slashes=False)
-@app.route('/python/<text>', strict_slashes=False)
-def pythoniscool(text='is cool'):
-    """displays word Python followed by text input"""
-    return 'Python ' + text.replace('_', ' ')
-
-
-@app.route('/number/<int:n>', strict_slashes=False)
-def isanumber(n):
-    """ displays statement only if integer input received"""
-    return f'{n} is a number'
-
-
-@app.route('/number_template/<int:n>', strict_slashes=False)
-def numbersandtemplates(n):
-    """ display a HTML page only if integer input received"""
-    return render_template('5-number.html', n=n)
-
-
-@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
-def number_odd_or_even(n):
-    """ display a different HTML page only if integer input received"""
-    if n % 2 == 0:
-        evenness = 'even'
-    else:
-        evenness = 'odd'
-    return render_template('6-number_odd_or_even.html', n=n, evenness=evenness)
-
-
-@app.route('/states_list', strict_slashes=False)
-def states_list():
-    """displays HTML page with sorted list of states with ID"""
-    states = sorted(list(storage.all("State").values()), key=lambda x: x.name)
-    return render_template('7-states_list.html', states=states)
-
-
 @app.route('/cities_by_states', strict_slashes=False)
 def cities_by_states():
     """displays HTML page with cities by states"""
